451a7603c556cd29786979250aa86a04ff66e2d1
sunecosuri <sunecosuri@pepabo.com>
2018-04-05 15:21:21 +0900

    add

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vue Component Boilerplate
+
+Boilerplate for Vue.js
+
+## 使い方
+```
+git clone git@github.com:sunecosuri/vue-component-boilerplate.git
+npm i
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{msg}}
+    <child></child>
+  </div>
+</template>
+
+<script>
+import Child from './components/Child.vue'
+export default {
+  components: {
+    Child
+  },
+  data() {
+    return { msg: '親 コンポーネント' }
+  }
+}
+</script>
